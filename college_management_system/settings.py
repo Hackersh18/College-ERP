7e86c002a927c3730423113100a3210558394612
Hackersh18 <dr.harshdayal@gmail.com>
2025-09-09 20:34:49 +0530

    chore: commit user changes in settings.py

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -29,11 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'f2zx8*lb*em*-*b+!&1lpp&$_9q9kmkar+l3x90do@s(+sr&x7'  # Consider using your secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# Read DEBUG from environment; defaults to False when not set
-DEBUG = str(os.environ.get('DEBUG', 'False')).lower() == 'true'
+
 
 # Hosts allowed to serve the app; comma-separated env var
-ALLOWED_HOSTS = [h for h in os.environ.get('ALLOWED_HOSTS', '*').split(',') if h]
+
   # Not recommended but useful in dev mode
 
 
@@ -111,23 +110,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-if not DEBUG:
-    AUTH_PASSWORD_VALIDATORS = []
-else:
-    AUTH_PASSWORD_VALIDATORS = [
-        {
-            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-        },
-    ]
+
 
 
 # Internationalization
